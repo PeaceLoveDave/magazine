8677ec1661499b2daffd60f13cfd0b0d9d0b51a1
PeaceLoveDave <david.j.lumb@gmail.com>
2014-09-16 17:09:02 -0400

    Initial commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,27 @@
+class ArticlesController < ApplicationController
+
+  def show
+  	@article = Article.find(params[:id])
+  end
+
+  def new
+  	@article = Article.new
+  end
+
+  def create
+  	@article = Article.create( article_params )
+
+  	redirect_to article_path(@article)
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  private
+  	def article_params
+  		params.require(:article).permit(:content, :title, :author, :category)
+  	end
+end
